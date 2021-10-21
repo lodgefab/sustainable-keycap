4c7abc8e0cd38c05ecc513ab0a1976da685c7f69
inside-hakumai <inside.rice164@gmail.com>
2021-10-22 00:10:50 +0900

    追加用ページ + Firebaseへの送信処理をざっくり実装

---
+++ lib/validation.ts
@@ -0,0 +1,72 @@
+import { colorTypeItems, plasticTypeItems } from '../types'
+import * as yup from 'yup'
+
+/**
+ * 素材登録フォームのバリデーション要件
+ */
+export const schema = yup
+  .object()
+  .shape({
+    // 廃プラ素材の画像: 画像は一度に1つまで、画像サイズは1MB以下
+    plasticImage: yup
+      .mixed()
+      .required()
+      // UI上、同時に選択できるファイルは1つのはずだが、何らかの理由で2つ以上のファイルが選択された場合は弾く
+      .test('fileCount', 'Only one file can be selected at a time.', (files) => files.length === 1)
+      // ファイルサイズが1MBを超えるものは弾く
+      // 記述したtest()の実行順は保証されず、ファイル数が1でない場合もここのテストが実行されてしまうため、
+      // ファイル数が1でない場合はファイルサイズのチェックは行わずここのテストは通す（上記のテストで弾かれるはず）
+      .test(
+        'fileSize',
+        'The maximum file size is 1MB.',
+        (files) => files.length !== 1 || files[0].size < 1000000
+      ),
+
+    // キーキャップの画像: 画像は一度に1つまで、画像サイズは1MB以下
+    keycapImage: yup
+      .mixed()
+      .required()
+      // UI上、同時に選択できるファイルは1つのはずだが、何らかの理由で2つ以上のファイルが選択された場合は弾く
+      .test('fileCount', 'Only one file can be selected at a time.', (files) => files.length === 1)
+      // ファイルサイズが1MBを超えるものは弾く
+      // 記述したtest()の実行順は保証されず、ファイル数が1でない場合もここのテストが実行されてしまうため、
+      // ファイル数が1でない場合はファイルサイズのチェックは行わずここのテストは通す（上記のテストで弾かれるはず）
+      .test(
+        'fileSize',
+        'The maximum file size is 1MB.',
+        (files) => files.length !== 1 || files[0].size < 1000000
+      ),
+
+    // 素材名: 入力必須
+    materialName: yup.string().required(),
+
+    // 色の種類: 入力必須、かつ選択肢から外れた入力は受け付けない
+    colorType: yup
+      .string()
+      .required()
+      .test('matchWithColorType', (value) => colorTypeItems.includes(value!)),
+
+    // プラスチックの種類: 入力必須、かつ選択肢から外れた入力は受け付けない
+    plasticType: yup
+      .string()
+      .required()
+      .test('matchWithPlasticType', (value) => plasticTypeItems.includes(value!)),
+
+    // 設定温度: 入力必須、正の整数
+    celsius: yup
+      .number()
+      .integer()
+      .min(0)
+      .transform((value, originalValue) => {
+        if (typeof originalValue === 'string' && originalValue === '') {
+          return null
+        }
+        return value
+      })
+      .nullable()
+      .required(),
+
+    // 備考: 特にバリデーション要件は無し
+    note: yup.string(),
+  })
+  .required()
---
+++ types.ts
@@ -0,0 +1,71 @@
+export const colorTypeItems: ReadonlyArray<string> = ['red', 'blue', 'green', 'black', 'white']
+export type ColorType = typeof colorTypeItems[number]
+
+export const plasticTypeItems: ReadonlyArray<string> = [
+  'plastic-a',
+  'plastic-b',
+  'plastic-c',
+  'plastic-d',
+]
+export type PlasticType = typeof plasticTypeItems[number]
+
+export interface Material {
+  id: string
+  materialName: string
+  colorType: 'red' | 'blue' | 'green' | 'black' | 'white'
+  plasticType: string
+  goodCount: number
+  plasticImageUrl: string
+  keycapImageUrl: string
+}
+
+export interface FirestoreMaterialDocument {
+  materialName: string
+  colorType: 'red' | 'blue' | 'green' | 'black' | 'white'
+  plasticType: string
+  goodCount: number
+}
+
+export interface ContentfulMaterialResponse {
+  materialName: string
+  plasticImage: File
+  keycapImage: File
+  colorType: ColorType
+  colorHex: string
+  plasticType: PlasticType
+  goodCount: number
+  celsius: number
+  note: string
+}
+
+export interface ContentfulMaterialRequest {
+  materialName: { 'en-US': string }
+  plasticImage: { 'en-US': any }
+  keycapImage: { 'en-US': any }
+  colorType: { 'en-US': ColorType }
+  colorHex: { 'en-US': string }
+  plasticType: { 'en-US': PlasticType }
+  goodCount: { 'en-US': number }
+  celsius: { 'en-US': number }
+  note: { 'en-US': string }
+}
+
+export interface RegisterForm {
+  plasticImage: FileList
+  keycapImage: FileList
+  materialName: string
+  colorType: string
+  plasticType: string
+  celsius: number
+  note: string
+}
+
+export interface RegisterRequestFromClient {
+  plasticImage: BinaryData
+  keycapImage: BinaryData
+  materialName: string
+  colorType: string
+  plasticType: string
+  celsius: string // 受け取るのは数値だが、FormData()の仕様上文字列で送られてくる
+  note: string
+}
