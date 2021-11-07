f8e6ae91b5bacd421a4f7d348a2e665ba36e6643
sorashin <naka52shin@gmail.com>
2021-11-08 07:25:19 +0900

    scroll trigger animationのセットアップ

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,49 +15,16 @@ const Layout = ({ children }: Props) => {
   const authState = useContext(AuthContext)
   const currentUser = getAuth().currentUser
 
-  const containerRef = useRef<HTMLDivElement>(null)
-
-  const size = useWindowSize()
-  const data = {
-    ease: 0.1,
-    curr: 0,
-    prev: 0,
-    rounded: 0,
-  }
-  const setBodyHeight = () => {
-    document.body.style.height = `${containerRef.current?.getBoundingClientRect().height}px`
-  }
-
-  // prev と currentのスクロール量の差分を徐々に無くしていく
-  const smoothScroll = useCallback(() => {
-    data.curr = window.scrollY
-    data.prev += (data.curr - data.prev) * data.ease
-    data.rounded = Math.round(data.prev * 100) / 100
-    containerRef.current!.style.transform = `translateY(-${data.rounded}px)`
-    requestAnimationFrame(() => smoothScroll())
-  }, [data])
-
-  useEffect(() => {
-    requestAnimationFrame(() => smoothScroll())
-  })
-
-  useEffect(() => {
-    setBodyHeight()
-  }, [size.height])
-
   return (
-    <Wrap>
+    <Container>
       <Header
         currentUser={currentUser}
         authState={authState}
         onLoginFunc={login}
         onLogoutFunc={logout}
       />
-      <Container ref={containerRef}>
-        {children}
-        <Footer />
-      </Container>
-    </Wrap>
+      {children}
+    </Container>
   )
 }
 
@@ -66,13 +33,4 @@ export default Layout
 const Container = styled.div`
   background-color: ${color.background.white};
   position: relative;
-`
-
-const Wrap = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
 `
