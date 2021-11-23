90edd74517cd7788f7e8c1c55f6a5753e692e72a
inside-hakumai <inside.rice164@gmail.com>
2021-11-23 18:12:52 +0900

    メタタグ周辺のリファクタリング

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,22 +12,19 @@ class MyDocument extends Document {
     ? `https://${process.env.VERCEL_URL}/images/photos/OGP.jpg`
     : `http://localhost:3000/images/photos/OGP.jpg`
 
+  description =
+    '#ANYCAPは、廃棄プラスチックを使ってキーキャップを自作するオープンソースコミュニティです。家庭やオフィスで出るプラゴミを原材料としたキーキャップを製作し、手法やデータを公開することを通じて、仲間の輪を広げる活動を行なっています。'
+
   render() {
     return (
       <Html>
         <Head>
-          <meta
-            name='description'
-            content='#ANYCAPは、廃棄プラスチックを使ってキーキャップを自作するオープンソースコミュニティです。家庭やオフィスで出るプラゴミを原材料としたキーキャップを製作し、手法やデータを公開することを通じて、仲間の輪を広げる活動を行なっています。'
-          />
+          <meta name='description' content={this.description} />
 
           {/* OGP関係タグ */}
           <meta property='og:site_name' content='#ANYCAP' />
           <meta property='og:image' content={this.previewImageUrl} />
-          <meta
-            property='og:description'
-            content='#ANYCAPは、廃棄プラスチックを使ってキーキャップを自作するオープンソースコミュニティです。家庭やオフィスで出るプラゴミを原材料としたキーキャップを製作し、手法やデータを公開することを通じて、仲間の輪を広げる活動を行なっています。'
-          />
+          <meta property='og:description' content={this.description} />
           <meta property='og:title' content='#ANYCAP' />
           <meta property='og:type' content='website' />
           <meta property='og:url' content='https://anycap.xyz' />
@@ -35,12 +32,8 @@ class MyDocument extends Document {
           <meta property='og:locale' content='en_US' />
 
           {/* Twitter関係タグ */}
-          <meta
-            name='twitter:description'
-            content='#ANYCAPは、廃棄プラスチックを使ってキーキャップを自作するオープンソースコミュニティです。家庭やオフィスで出るプラゴミを原材料としたキーキャップを製作し、手法やデータを公開することを通じて、仲間の輪を広げる活動を行なっています。'
-          />
+          <meta name='twitter:description' content={this.description} />
           <meta name='twitter:image' content={this.previewImageUrl} />
-          <meta name='twitter:app:country' content='' />
           <meta name='twitter:card' content='jp' />
           <meta name='twitter:title' content='#ANYCAP' />
           {/*<meta name='twitter:site' content='' />*/}
