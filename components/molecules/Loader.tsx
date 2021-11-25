c9c22ea10c8e1c3c25e95011dabdbdc221ca2cd5
sorashin <naka52shin@gmail.com>
2021-11-25 13:52:58 +0900

    loaderの画像を差し替え

--- components/molecules/Loader.tsx
+++ components/molecules/Loader.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { color, zIndex } from '../../styles'
-import Image from 'next/image'
+import { color, media, zIndex } from '../../styles'
 
 type Props = {}
 
 export const Loader: React.VFC<Props> = ({}) => {
   return (
     <Container className='loader'>
-      <Image width={400} height={116} src={'/images/logotype_white.svg'} alt='logo' />
+      <APNG src={'/images/loader.png'} alt='logo' />
     </Container>
   )
 }
@@ -23,4 +22,12 @@ const Container = styled.div`
   background-color: ${color.background.dark};
   z-index: ${zIndex.loader};
   overflow: hidden;
+`
+const APNG = styled.img`
+  width: 320px;
+  height: 320px;
+  ${media.mdsp} {
+    width: 180px;
+    height: 180px;
+  }
 `
